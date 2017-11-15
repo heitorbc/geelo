91a4d0a600f5b74433cbdd1d2e008a532a834499
Heitor BC <heitorhbc@gmail.com>
2017-11-15 22:14:22 +0000

    configurado testes para rodar no travis

--- features/steps/login.py
+++ features/steps/login.py
@@ -19,9 +19,9 @@ def step_impl(context):
 def step_impl(context):
     br = context.browser
     
-    br.get('https://geelo.herokuapp.com/') 
+    #br.get('https://geelo.herokuapp.com/')
     #br.get('$IP:$PORT')
-    #br.get(context.base_url + '/login')
+    br.get(context.base_url + '/login')
     
     #br.get_screenshot_as_file('./screenshot.png')
     
